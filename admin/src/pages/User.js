c31e772455528d2cd6136a81330949d459201732
pratikwadke02 <82306536+pratikwadke02@users.noreply.github.com>
2022-09-14 03:01:57 +0530

    admin done

--- admin/src/pages/User.js
+++ admin/src/pages/User.js
@@ -76,7 +76,7 @@ export default function User() {
 
   useEffect(() => {
     const getUserData = async () => {
-      const { data } = await axios.get('http://yogajagriti.com:5000/api/yoga/getAllUsers');
+      const { data } = await axios.get('http://localhost:5000/api/food-ecom/getAllUsers');
       setUsers(data);
       console.log(users);
     };
